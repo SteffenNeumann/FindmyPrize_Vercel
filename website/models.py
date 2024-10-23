4b936760e51b834cb362e67d6ed9ce19cbd42370
Mastercaution <neumann-steffen@web.de>
2024-10-23 07:06:03 +0200

    improvments

--- website/models.py
+++ website/models.py
@@ -1,7 +1,10 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
+from geopy.exc import GeocoderTimedOut
+from geopy.geocoders import Nominatim
+from functools import partial
+import time
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +19,11 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+class ScraperResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, ScraperResult
 from . import db
-import json
+from .scrapper import run_scraper
 
 views = Blueprint('views', __name__)
 
@@ -10,30 +10,40 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+    if request.method == 'POST':
+        if 'note' in request.form:
+            # Existing note functionality
+            note = request.form.get('note')
+            if len(note) < 1:
+                flash('Note is too short!', category='error')
+            else:
+                new_note = Note(data=note, user_id=current_user.id)
+                db.session.add(new_note)
+                db.session.commit()
+                flash('Note added!', category='success')
+        elif 'run_scraper' in request.form:
+            # Scraper functionality
+            city = request.form.get('city')
+            country = request.form.get('country')
+            results = run_scraper(city, country)
+            for result in results:
+                new_result = ScraperResult(data=result, user_id=current_user.id)
+                db.session.add(new_result)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Scraper results added!', category='success')
 
-    return render_template("home.html", user=current_user)
-
+    notes = Note.query.filter_by(user_id=current_user.id).all()
+    scraper_results = ScraperResult.query.filter_by(user_id=current_user.id).all()
+    return render_template("home.html", user=current_user, notes=notes, scraper_results=scraper_results)
 
 @views.route('/delete-note', methods=['POST'])
+
 def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
-
-
+     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+     noteId = note['noteId']
+     note = Note.query.get(noteId)
+     if note:
+         if note.user_id == current_user.id:
+             db.session.delete(note)
+             db.session.commit()#
+     return jsonify({})
